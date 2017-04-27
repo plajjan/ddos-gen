c551e00fbcc1ef72aa048fcc8b8eae78a017e818
Kristian Larsson <kristian@spritelink.net>
2017-04-27 17:14:02 +0200

    Add latest version from cgraf

--- ddos-gen.py
+++ ddos-gen.py
@@ -10,6 +10,10 @@
 # nor any ND6 process fails due to overloading the controlplane
 # use at own risk
 
+# install BGPOpen
+# 1) download bgp.py from here: https://github.com/levigross/Scapy/blob/master/scapy/contrib/bgp.py
+# 2) copy it here: ./usr/local/lib/python2.7/dist-packages/scapy/contrib/bgp.py
+
 
 from scapy.all import *
 from scapy.contrib.bgp import BGPOpen
@@ -30,8 +34,9 @@ parser = argparse.ArgumentParser(description=my_description)
 parser.add_argument('-dmac','--dmac', help='defines destination mac , default 22:22:22:22:22:22', default='22:22:22:22:22:22', required=False)
 parser.add_argument('-smac','--smac', help='defines initial source mac , default 23:22:22:22:22:22', default='23:22:22:22:22:22', required=False)
 parser.add_argument('-dip','--dip', help='defines destination IPv6, default 2001:db8:100::1', default='2001:db8:100::1', required=False)
-parser.add_argument('-sip','--sip', help='defines initial source IPv6, default 2001:db8:200::1', default='2001:db8:200::1', required=False)
+parser.add_argument('-sip','--sip', help='defines initial source IPv6, default 2003:1c08:20:0ff::1', default='2003:1c08:20:ff::1', required=False)
 parser.add_argument('-subs','--subs', help='total amount of subscribers to use. For each subscriber the tool will create one packet per selected protocol', type=int, default=2, required=False)
+parser.add_argument('-sps','--sources_per_subscriber', help='As the subscribers have a rather large cidr-block, this knob turns on multiple sources per subscriber. Default is 1', type=int, default=1, required=False)
 parser.add_argument('-pcap_file','--pcap_file', help='if provided, the resulting pcap-file will be saved under provided name, default=ddos.pcap', default=outfile, required=False)
 
 # vlans
@@ -46,8 +51,8 @@ parser.add_argument("--dhcp_solicit", default=False, action="store_true" , help=
 # all-knob
 parser.add_argument("--all", default=False, action="store_true" , help="set this knob to turn on all available tests (bfd,bgp,icmp,..)")
 
-# distributed or non-distributed
-parser.add_argument("--distributed", default=False, action="store_true" , help="if set, then the packets will be sourced from different src-ip. dafault: false, means all packets use a single src-ip.")
+# offset / distributed or non-distributed
+parser.add_argument("--offset", default=0x0, type=str, help="for terastream use 0x1000000000000000000 as offset between the vlans. SIP: default starting value could be 2003:1c08:20:0ff::/64 i.e. 0x20031c08002001ff0000000000000000 SIP prefix = start + (customer_id * 0x1000000000000000000) So the result would be that for the 1st customer you have: VLAN 2001 SIP prefix  2003:1c08:20:1ff::/64 2nd customer: VLAN 2002 SIP  prefix 2003:1c08:20:2ff::/64")
 
 
 # last statement args
@@ -84,13 +89,10 @@ if args.all == True:
     args.dhcp_solicit = True
     args.icmp_request = True
 
-# end-ip
-help=IPAddress(args.sip) + args.subs -1
-if args.distributed == True:
-   end_ip= str(help)
-else:
-   end_ip=args.sip
-
+offset = str(args.offset)
+ip_increment= (args.subs-1) * int(offset,16)
+end_ip=IPAddress(args.sip)
+end_ip = end_ip + ip_increment
 
 print "."
 # print used values
@@ -120,7 +122,10 @@ if args.icmp_request == True:
 print "."
 print "distributed or non-distributed pattern"
 print "--------------------------------------"
-print "distributed      :", args.distributed 
+if args.offset <> 0:
+  print "using distributed traffic pattern, based on offset given"
+else:
+  print "all generated traffic using static/single src-ip" 
 print "."
 print "."
 print "."
@@ -128,6 +133,7 @@ print "starting populating the pcap-file"
 print "================================="
  
 ## traffic generation
+## this is the outer-loop, defined by the number of total subscribers
 packet_count=0
 for x in range(0,args.subs):
 
@@ -141,15 +147,6 @@ for x in range(0,args.subs):
    # modifier and variance is defined here
    dhcp_dst = "ff02::1:2"
 
-   if args.distributed == True:
-     # l3 src address gets incremented with each loop
-     src_ip=IPAddress(args.sip) + x
-     l3=IPv6(src=str(src_ip),dst=args.dip)
-   else:
-     # l3 src keeps static
-     l3=IPv6(src=args.sip,dst=args.dip)
-     src_ip=IPAddress(args.sip)
-
    # udp/tcp
    src_port_static   = 1024
    src_port_variance = 1024+x
@@ -159,89 +156,94 @@ for x in range(0,args.subs):
    bfd_control       = 3784
    bfd_echo          = 3785
    bfd_srcport       = random.randint(49152,65535)
+   
+   # this is the inner loop, reiterating through the sources_per_subscriber
+   for y in range (0,args.sources_per_subscriber):
+	   # l3 src address gets incremented with each outer and inner loop
+	   ip_increment= x * int(offset,16) + y
+	   src_ip=IPAddress(args.sip)
+	   src_ip = src_ip + ip_increment
+	   l3=IPv6(src=str(src_ip),dst=args.dip)
+	   # l2 header
+	   ## check if packets shall be untagged, single-tagged or double-tagged
+	   ## with each subscriber (args.subs) the vlan-id is incremented
+	   if args.vid <> 'False':
+	       # packets are either tagged or double-tagged
+	       help=args.vid.split(",")
+	       # single tagged
+	       if len(help) == 1:
+		   # make sure with each run the svlan gets incremented
+		   svlan=int(help[0])+x
+		   print "single tagged"
+		   l2=Ether(src=args.smac, dst=args.dmac)/Dot1Q(vlan=svlan)
+		   l2_mcast=Ether(src=args.smac, dst=multicast_MAC)/Dot1Q(vlan=svlan)
+		   l2.show()
 
-   # l2 header
-   ## check if packets shall be untagged, single-tagged or double-tagged
-   ## with each subscriber (args.subs) the vlan-id is incremented
-   if args.vid <> 'False':
-       # packets are either tagged or double-tagged
-       help=args.vid.split(",")
-       # single tagged
-       if len(help) == 1:
-           # make sure with each run the svlan gets incremented
-           svlan=int(help[0])+x
-           print "single tagged"
-           l2=Ether(src=args.smac, dst=args.dmac)/Dot1Q(vlan=svlan)
-           l2_mcast=Ether(src=args.smac, dst=multicast_MAC)/Dot1Q(vlan=svlan)
-           l2.show()
+	       if len(help) == 2:
+		   # make sure with each run the svlan gets incremented
+		   svlan=int(help[0])+x
+		   cvlan=int(help[1])
+		   print "double tagged"
+		   l2=Ether(src=args.smac, dst=args.dmac)/Dot1Q(vlan=svlan)/Dot1Q(vlan=cvlan)
+		   l2_mcast=Ether(src=args.smac, dst=multicast_MAC)/Dot1Q(vlan=svlan)/Dot1Q(vlan=cvlan)
+	   else:
+	       # args.vid is default False, hence untagged
+	       # print "packet is untagged"
+	       l2=Ether(src=args.smac, dst=args.dmac, type=0x86dd)
+	       l2_mcast=Ether(src=args.smac, dst=multicast_MAC, type=0x86dd)
 
-       if len(help) == 2:
-           # make sure with each run the svlan gets incremented
-           svlan=int(help[0])+x
-           cvlan=int(help[1])
-           print "double tagged"
-           l2=Ether(src=args.smac, dst=args.dmac)/Dot1Q(vlan=svlan)/Dot1Q(vlan=cvlan)
-           l2_mcast=Ether(src=args.smac, dst=multicast_MAC)/Dot1Q(vlan=svlan)/Dot1Q(vlan=cvlan)
-   else:
-       # args.vid is default False, hence untagged
-       # print "packet is untagged"
-       l2=Ether(src=args.smac, dst=args.dmac, type=0x86dd)
-       l2_mcast=Ether(src=args.smac, dst=multicast_MAC, type=0x86dd)
+	   # bgp
+	   # for bgp two different flavors are beeing launched
+	   # first a single source is generating multiple sockets to same target
+	   # second multiple sources are firing against same target
 
-   # bgp
-   # for bgp two different flavors are beeing launched
-   # first a single source is generating multiple sockets to same target
-   # second multiple sources are firing against same target
+	   if args.bgp == True:
+	       if x == 0:
+		   print "starting: bgp traffic generation "
+	       # launching multiple sockets from same src-IP
+	       packet=l2/l3/TCP(sport=src_port_variance,dport=179)/BGPOpen()
+	       packet_count += 1
+	       pktdump.write(packet)
 
-   if args.bgp == True:
-       if x == 0:
-           print "starting: bgp traffic generation "
-       # launching multiple sockets from same src-IP
-       packet=l2/l3/TCP(sport=src_port_variance,dport=179)/BGPOpen()
-       packet_count += 1
-       pktdump.write(packet)
+	   # dhcp solicit
+	   if args.dhcp_solicit == True:
+	       if x == 0:
+		   print "starting: dhcp-solicit generation "
+	       sol = DHCP6_Solicit()
+	       iana = DHCP6OptIA_NA()
+	       rc = DHCP6OptRapidCommit()
+	       et= DHCP6OptElapsedTime()
+	       cid = DHCP6OptClientId()
+	       opreq = DHCP6OptOptReq()
+	       packet = l2_mcast/IPv6(src=str(src_ip),dst=dhcp_dst)/UDP(sport=dhcp_sport,dport=dhcp_dport)/sol/iana/rc/et/cid/opreq
+	       packet_count += 1
+	       pktdump.write(packet)
 
-   # dhcp solicit
-   if args.dhcp_solicit == True:
-       if x == 0:
-           print "starting: dhcp-solicit generation "
-       sol = DHCP6_Solicit()
-       iana = DHCP6OptIA_NA()
-       rc = DHCP6OptRapidCommit()
-       et= DHCP6OptElapsedTime()
-       cid = DHCP6OptClientId()
-       opreq = DHCP6OptOptReq()
-       packet = l2_mcast/IPv6(src=str(src_ip),dst=dhcp_dst)/UDP(sport=dhcp_sport,dport=dhcp_dport)/sol/iana/rc/et/cid/opreq
-       packet_count += 1
-       pktdump.write(packet)
+	   # icmp_request
+	   if args.icmp_request == True:
+	       if x == 0:
+		   print "starting: icmp echo-request"
+	       packet=l2/l3/ICMPv6EchoRequest()
+	       packet_count += 1
+	       pktdump.write(packet)
 
-   # icmp_request
-   if args.icmp_request == True:
-       if x == 0:
-           print "starting: icmp echo-request"
-       packet=l2/l3/ICMPv6EchoRequest()
-       packet_count += 1
-       pktdump.write(packet)
-
-   # bfd
-   # note: its not a real bfd-packet, its just using the port-range for bfd
-   if args.icmp_request == True:
-       if x == 0:
-           print "starting: BFD"
-       # generating bfd_control packets
-       packet=l2/l3/UDP(sport=bfd_srcport, dport=bfd_control)
-       packet_count += 1
-       pktdump.write(packet)
-       # bfd_multihup 
-       packet=l2/l3/UDP(sport=bfd_srcport, dport=bfd_multihop)
-       packet_count += 1
-       pktdump.write(packet)
-       # bfd echo
-       packet=l2/l3/UDP(sport=bfd_srcport, dport=bfd_echo)
-       packet_count += 1
-       pktdump.write(packet)
-
-
+	   # bfd
+	   # note: its not a real bfd-packet, its just using the port-range for bfd
+	   if args.bfd == True:
+	       if x == 0:
+		   print "starting: BFD"
+	       # generating bfd_control packets
+	       packet=l2/l3/UDP(sport=bfd_srcport, dport=bfd_control)
+	       packet_count += 1
+	       pktdump.write(packet)
+	       # bfd_multihup 
+	       packet=l2/l3/UDP(sport=bfd_srcport, dport=bfd_multihop)
+	       packet_count += 1
+	       pktdump.write(packet)
+	       # bfd echo
+	       packet=l2/l3/UDP(sport=bfd_srcport, dport=bfd_echo)
+	       packet_count += 1
+	       pktdump.write(packet)
 print "===="
 print "writing file ", args.pcap_file
 pktdump.close()
